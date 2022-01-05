0b55c708d1884f94a1bced64c004e0a79a1800f2
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2022-01-05 11:50:56 -0500

    Add player & board creation

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,1 +1,11 @@
+# frozen_string_literal: true
 
+require_relative 'player'
+require_relative 'board'
+
+puts "Let's play a game of Tic-Tac-Toe!"
+player1 = Player.new('Player 1', 'X')
+player2 = Player.new('Player 2', 'O')
+
+board = Board.new
+board.display
